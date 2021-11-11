84846653583ab81d8aa8439e5f30a1a9f3a59f1b
Nicholas <nicholas.sumanto@ti.ukdw.ac.id>
2021-11-11 16:35:08 +0700

    3_A_71210684.py

---
+++ 3_A_71210684.py
@@ -0,0 +1,9 @@
+#Belanja
+a=str(input("Masukan daftar belanja 1: "))
+b=str(input("Masukan daftar belanja 2: "))
+#Tambahan Belanja
+x=str(input("Tambahkan data ke daftar belanga 1: "))
+y=str(input("Tambahkan data ke daftar belanga 2: "))
+#Daftar Belanja
+print("Daftar belanja 1 adalah ",a,",",x,sep="")
+print("Daftar belanja 2 adalah ",b,",",y,sep="")
