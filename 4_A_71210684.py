28818e8409a51ef12471bbcd1a912d46dd11fcf7
Nicholas <nicholas.sumanto@ti.ukdw.ac.id>
2021-11-11 16:35:38 +0700

    4_A_71210684.py

---
+++ 4_A_71210684.py
@@ -0,0 +1,18 @@
+#Pesanan
+print("=====MASUKKAN JUMLAH MAKANAN YANG DIPESAN=====")
+I=int(input("IKAN BAKAR     Rp 25.000,00  :"))
+E=int(input("ES DOGER       Rp 6.000,00   :"))
+R=int(input("RUJAK CINGUR   Rp 8.000,00   :"))
+#Total
+TI=25000*I
+TE=6000*E
+TR=8000*R
+print("=====TOTAL=====")
+print("TOTAL IKAN BAKAR     : Rp",TI)    
+print("TOTAL ES DOGER       : Rp",TE)
+print("TOTAL RUJAK CINGUR   : Rp",TR)
+#Jumlah total Bayar
+JT=TI+TE+TR
+print("Jumlah total biaya yang harus dibayar adalah Rp",JT)      
+      
+
